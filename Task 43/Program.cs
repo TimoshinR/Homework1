15c6485b6f83ca69210b828677c18cd43154b5d6
TimoshinR <108608476+TimoshinR@users.noreply.github.com>
2022-08-09 10:21:59 +0300

    Add files via upload

---
+++ Task 43/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; 5,5)
+
+double[,] coeff = new double[2, 2];
+double[] point = new double[2];
+
+void getCoeff()
+{
+  for (int i = 0; i < coeff.GetLength(0); i++)
+  {
+    Console.Write($"Введите коэффициенты для {i+1}-го уравнения:\n");
+    for (int j = 0; j < coeff.GetLength(1); j++)
+    {
+      if(j==0) Console.Write($"Введите коэффициент k: ");
+      else Console.Write($"Введите коэффициент b: ");
+      coeff[i,j] = Convert.ToInt32(Console.ReadLine());
+    }
+  }
+}
+
+double[] getAct(double[,] coeff)
+{
+  point[0] = (coeff[1,1] - coeff[0,1]) / (coeff[0,0] - coeff[1,0]);
+  point[1] = point[0] * coeff[0,0] + coeff[0,1];
+  return point;
+}
+
+void getResult(double[,] coeff)
+{
+  if (coeff[0,0] == coeff[1,0] && coeff[0,1] == coeff[1,1]) 
+  {
+    Console.Write($"Прямые совпадают");
+  }
+  else if (coeff[0,0] == coeff[1,0] && coeff[0,1] != coeff[1,1]) 
+  {
+    Console.Write($"Прямые параллельны");
+  }
+  else 
+  {
+    getAct(coeff);
+    Console.Write($"Точка пересечения прямых: ({point[0]} ; {point[1]})");
+  }
+}
+
+getCoeff();
+getResult(coeff);
+Console.WriteLine();
