67f34729b8d31dc318031442edf02bbf59674f23
TimoshinR <108608476+TimoshinR@users.noreply.github.com>
2022-08-15 16:42:03 +0300

    Add files via upload

---
+++ Task 58/Program.cs
@@ -0,0 +1,76 @@
+﻿// Задача 58: 
+// Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, заданы 2 массива:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// и
+// 1 5 8 5
+// 4 9 4 2
+// 7 2 2 6
+// 2 3 4 7
+// Их произведение будет равно следующему массиву:
+// 1 20 56 10
+// 20 81 8 6
+// 56 8 4 24
+// 10 6 24 49
+
+Console.Clear();
+
+Console.Write("Введите число строк матриц m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число столбцов матриц n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] firstMartrix = new int[m, n];
+FillArray(firstMartrix);
+Console.WriteLine($"\nПервая матрица:");
+PrintArray(firstMartrix);
+
+int[,] secomdMartrix = new int[m, n];
+FillArray(secomdMartrix);
+Console.WriteLine($"\nВторая матрица:");
+PrintArray(secomdMartrix);
+
+int[,] resultMatrix = new int[m,n];
+
+MatrixMult(firstMartrix, secomdMartrix, resultMatrix);
+Console.WriteLine($"\nПроизведение матриц:");
+PrintArray(resultMatrix);
+
+void MatrixMult(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatrix)
+{
+  for (int i = 0; i < resultMatrix.GetLength(0); i++)
+    {
+    for (int j = 0; j < resultMatrix.GetLength(1); j++)
+        {
+            int mult = 0;
+            mult = firstMartrix[i,j] * secomdMartrix[i,j];
+            resultMatrix[i,j] = mult;
+        }
+    }
+}
+
+void FillArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(1, 10);
+    }
+  }
+}
+
+void PrintArray (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i,j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
